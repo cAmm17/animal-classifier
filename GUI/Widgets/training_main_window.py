25d0e6d5d0581d755adf0380abe2db1fba2bbfae
Courtney Amm <ammcourtney0@gmail.com>
2023-01-22 19:07:58 -0500

    Moved training on to separate thread

--- GUI/Widgets/training_main_window.py
+++ GUI/Widgets/training_main_window.py
@@ -3,6 +3,25 @@ import sys
 from PySide6 import QtCore, QtWidgets, QtGui
 from ..algos.image_classifier import ImageClassifier
 
+class Worker(QtCore.QObject):
+    """This class is the worker thread used to run the image classifier"""
+    finished = QtCore.Signal()
+    progress = QtCore.Signal(int)
+
+    def __init__(self, classifier, epochs) -> None:
+        super().__init__()
+        self.classifier = classifier
+        self.epochs = epochs
+
+
+    def run(self):
+        # Run the training function for 1 epoch in a loop so that progress can be incrementally reported
+        for i in range(self.epochs):
+            self.classifier.train_model(1)
+            self.progress.emit(i + 1)
+        self.finished.emit()
+
+
 class TrainingMainWindow(QtWidgets.QWidget, Ui_Form):
     def __init__(self,) -> None:
         super().__init__()
@@ -26,9 +45,18 @@ class TrainingMainWindow(QtWidgets.QWidget, Ui_Form):
 
         # add else with error popup here later to indicate the number is out of range/not a number
         if num.isdigit() and int(num) > 0 and int(num) < 100:
-            #need to thread off and read in stdout using q text stream
-            # readIn = QtCore.QTextStream(sys.stdout)
-            self.image_classifier.train_model(int(num))
+            # run the classifier on a separate thread so that the GUI doesn't freeze up
+            self.thread = QtCore.QThread()
+            self.worker = Worker(self.image_classifier, int(num))
+
+            self.worker.moveToThread(self.thread)
+
+            self.thread.started.connect(self.worker.run)
+            self.worker.finished.connect(self.thread.quit)
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+
+            self.thread.start()
 
     
 
